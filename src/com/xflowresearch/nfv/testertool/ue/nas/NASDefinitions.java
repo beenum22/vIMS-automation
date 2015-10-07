993de9138568c63ba2dff1589c2ad6d6eb1a6588
Kashmala Tahseen <kashmalatahseenkhan@gmail.com>
2015-10-07 09:30:48 +0500

    NAS Functionality added to: ue.nas package
    includes the classes:
    NASDefinitions
    NASImplementation
    ReadFile

---
+++ src/com/xflowresearch/nfv/testertool/ue/nas/NASDefinitions.java
@@ -0,0 +1,343 @@
+//NAC PROTOCOL DEFINITIONS
+
+package com.xflowresearch.nfv.testertool.ue.nas;
+public class NASDefinitions {
+
+		public enum MobilityManagementMessageType
+		//for the case when Protocol Discriminator = EPS Mobility Management Message
+		{
+			AttachRequest("01000001"),
+			AttachAccept("42"),
+			AttachComplete("43"),
+			AttachReject("44"),
+			DetachRequest("45"),
+			DetachAccept("46"),
+			TrackingAreaUpdateRequest("48"),
+			TrackingAreaUpdateAccept("49"),
+			TrackingAreaUpdateComplete("4A"),
+			TrackingAreaUpdateReject("4B"),
+			ExtendedServiceRequest("4C"),
+			ServiceReject("4D"),
+			GUTIReallocationCommand("50"),
+			GUTIReallocationComplete("51"),
+			AuthenticationRequest("52"),
+			AuthenticationResponse("01010011"),
+			AuthenticationReject("54"),
+			AuthenticationFailure("5C"),
+			IdentityRequest("55"),
+			IdentityResponse("56"),
+			SecurityModeCommand("5D"),
+			SecurityModeComplete("5E"),
+			SecurityModeReject("5F"),
+			EMMStatus("60"),
+			EMMInformation("61"),
+			DownlinkNASTransport("62"),
+			UplinkNASTransport("63"),
+			CSServiceNotification("64"),
+			DownlinkGenericNASTransport("68"),
+			UplinkGenericNASTransport("69");
+			
+			private String hexValue;
+			
+	
+			private MobilityManagementMessageType(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+		}
+		
+		
+		public enum EPSSessionManagementMessageType{  ////for the case when Protocol Discriminator = EPS Session Management Message Type
+			
+			ActivateDefaultEPSBearerContextRequest(""),
+			ActivateDefaultEPSBearerContextAccept(""),
+			ActivateDefaultEPSbearerContextReject(""),
+			ActivateDedicatedEPSbearerContextRequest(""),
+			ActivateDedicatedEPSbearerContextAccept(""),
+			ActivateDedicatedEPSbearercontextAeject(""),
+			ModifyEPSBearercontextrequest(""),
+			ModifyEPSBearercontextaccept(""),
+			ModifyEPSBearercontextreject(""),
+			DeactivateEPSBearercontextrequest(""),
+			DeactivateEPSBearerContextaccept(""),
+			PDNconnectivityRequest(""),
+			PDNconnectivityReject(""),
+			PDNdisconnectRequest(""),
+			PDNdisconnectReject(""),
+			BearerresourceAllocationRequest(""),
+			Bearerresourceallocationreject(""),
+			Bearerresourcemodificationrequest(""),
+			Bearerresourcemodificationreject(""),
+			ESMinformationrequest(""),
+			ESMInformationResponse("DA"),
+			Notification(""),
+			ESMstatus("");
+			
+			
+			private String hexValue;
+			
+			
+			private EPSSessionManagementMessageType(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+			
+		}
+	
+		
+		
+		
+		public enum SecurityHeaderType
+		{
+			PlainNASMessage("0000"),
+			IntegrityProtected("0001"),
+			IntegrityProtectedCiphered("0010"),
+			IntegrityProtectedwithnewEPSSecurityContext("0011"),
+			ServiceRequestMessageSecurityHeader("1000"),
+			SecurityheaderforServiceRequest("1100");
+			
+			//1100, 1111 These values are not used in this version of the protocol.
+			//to If received they shall be interpreted as '1100'. )
+			
+			
+			private String hexValue;
+			
+			
+			private SecurityHeaderType(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+		}
+		
+		
+		
+		
+		public enum ProtocolDiscriminatorValue{
+			
+			GroupCallControl("0000"),
+			BroadcastCallControl("0001"),
+			EPSSessionManagementMessage("0010"),
+			CallControl("0011"),
+			GTTP("0100"),
+			MobilityManagementMessage("0101"),
+			RRMMessage("0110"),
+			EPSMobilityManagementMessage("0111"),
+			GPRSMobilityManagementMessage("1000"),
+			SMSMessage("1001"),
+			GPRSSessionManagementMessage("1010"),
+			NonCallRelatedSSMessage("1011"),
+			LocationService("1100"),
+			ReservedforExtensionofPDtoOneOctetLength("1110"),
+			UsedByTestProcedure("1111");
+			
+			private String hexValue;
+			
+			
+			private ProtocolDiscriminatorValue(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+		}
+		
+		
+		
+		public enum TypeofSecurityContextFlag{       //4th bit in the octet
+			
+			NativeSecurityContext("0"),
+			MappedSecurityContext("1");
+			
+			
+			
+			private String hexValue;
+			private TypeofSecurityContextFlag(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+			
+			//TSC does not apply for NAS key set identifier value "7".
+		}
+		
+		
+		
+		public enum KeySetIdentifier
+		{
+	
+			NoKeyAvailable ("111");
+			
+			private String hexValue;
+			
+			private KeySetIdentifier(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+			
+		}
+		
+		
+		public enum SpareBit
+		{
+			Spare("0");
+			
+			private String hexValue;
+			
+			private SpareBit(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+			
+		}
+		
+		
+		public enum EPSAttachType{
+			
+			EPSAttach("001"),
+			CombinedEPSIMSIAttach("010"),
+			EPSEmergencyAttach("110"),
+			Reserved("7");
+			//All other values are unused and shall be interpreted as "EPS attach", if received by the network
+			
+			private String hexValue;
+			
+			private EPSAttachType (final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+			
+			
+			
+			
+		}
+		
+		
+		public enum TypeofIdentity
+		{
+			IMSI("1"),
+			GUTI("2"),
+			IMEI("3");
+			
+			//All other values are reserved
+			
+			private String hexValue;
+			
+			private TypeofIdentity (final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+		}
+		
+		
+		
+		public enum OddEvenIndication{
+			
+			odd("0"),
+			even("1");
+			
+			private String hexValue;
+			private OddEvenIndication(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+		}
+		
+		
+		
+		//NAS Security algorithms information elements
+		public enum TypeofIntegrityProtectionAlgorithm {
+			EIA0("0"),
+			EIA1("1"),
+			EIA2("2"),
+			EIA3("3"),
+			EIA4("5"),
+			EIA5("6"),
+			EIA6("7"),
+			EIA7("8");
+			
+
+			private String hexValue;
+			
+			private TypeofIntegrityProtectionAlgorithm (final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+			
+		}
+		
+		public enum TypeofCipheringAlgorithm {
+			EEA0("0"),
+			EEA1("1"),
+			EEA2("2"),
+			EEA3("3"),
+			EEA4("5"),
+			EEA5("6"),
+			EEA6("7"),
+			EEA7("8");
+			
+			
+			private String hexValue;
+			
+			private TypeofCipheringAlgorithm (final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+			
+		}
+		
+		public enum TMSIStatusBit{
+			
+			NoValidTMSIavailable("0"),
+			ValidTMSIavailable("1");
+			
+			private String hexValue;
+			
+			private TMSIStatusBit(final String hexValue) {
+				this.hexValue = hexValue;
+			}
+			
+			public String getHexCode(){
+				return hexValue;
+			}
+			
+		}
+		
+		
+}
+	
+	
+	
+
---
+++ src/com/xflowresearch/nfv/testertool/ue/nas/NASImplementation.java
@@ -0,0 +1,357 @@
+package com.xflowresearch.nfv.testertool.ue.nas;
+
+import java.io.IOException;
+
+
+public class NASImplementation {
+
+private String AttachRequest;
+private String AuthRequest;
+private String AuthResponse;
+private String AuthReject;
+private String AuthFailure;
+private String SecureModeCommand;
+private String SecurityModeReject;
+private String SecurityModeComplete;
+private String AttachReject;
+private String ESMInfoRequest;
+private String ESMInfoResponse;
+private String AttachComplete;
+
+public void NASInit(String AttachReq, String AuthReq,String AuthResp,String AuthRej,String AuthFail,String SecureModeCmd,String SecurityModeRej,String SecurityModeComp,String AttachRej,String ESMInfoReq,String ESMInfoResp,String AttachComp)
+{
+	AttachRequest=AttachReq;
+	AuthRequest=AuthReq;
+	AuthReject=AuthRej;
+	AuthFailure=AuthFail;
+	AttachReject=AttachRej;
+	SecureModeCommand=SecureModeCmd;
+	SecurityModeReject=SecurityModeRej;
+	ESMInfoRequest=ESMInfoReq;
+	ESMInfoResponse=ESMInfoResp;
+	AttachComplete=AttachComp;
+	SecurityModeComplete=SecurityModeComp;
+}
+
+public void parametersetter(String Filepath) throws IOException{
+	
+	
+    ReadFile Reader=new ReadFile();
+	String Str=Reader.readCommands(Filepath);
+	String [] Command=Str.split("\n");
+	
+
+	AttachRequest=Command[0];
+	AuthRequest=Command[1];
+	AuthResponse=Command[2];
+	AuthReject=Command[3];
+	AuthFailure=Command[4];
+	SecureModeCommand=Command[5];
+	SecurityModeReject=Command[6];
+	SecurityModeComplete=Command[7];
+	AttachReject=Command[8];
+	ESMInfoRequest=Command[9];
+	ESMInfoResponse=Command[10];
+	AttachComplete=Command[11];
+	
+	
+	//Debugging
+	//System.out.println("debug test start");
+	//System.out.println(Str);
+	//System.out.println(AttachRequest);
+	//System.out.println(AuthRequest);
+	//System.out.println(AuthResponse);
+	//System.out.println(AuthReject);
+	//System.out.println("test ends");
+
+}
+
+ public void NASAttachRequest(String packetval)
+	{
+		
+	
+	 //String value1="710BF600F1100001011234567802C0C000050201D031D15200F1100001"; //representing a particular UE
+		
+	    String value1=packetval;
+	    
+	    String firstbyte=(NASDefinitions.SecurityHeaderType.PlainNASMessage.getHexCode()
+				+ NASDefinitions.ProtocolDiscriminatorValue.EPSMobilityManagementMessage.getHexCode()); //first
+	   
+	    firstbyte='0'+Integer.toHexString(Integer.parseInt(firstbyte,2));
+	   
+		   
+		String secondbyte= NASDefinitions.MobilityManagementMessageType.AttachRequest.getHexCode(); 
+		
+		secondbyte=Integer.toHexString(Integer.parseInt(secondbyte,2));
+	    
+	    String thirdByte=NASDefinitions.TypeofSecurityContextFlag.NativeSecurityContext.getHexCode()+ NASDefinitions.KeySetIdentifier.NoKeyAvailable.getHexCode()+
+	    		NASDefinitions.SpareBit.Spare.getHexCode()+NASDefinitions.EPSAttachType.EPSAttach.getHexCode();   
+	    
+	    thirdByte= Integer.toHexString(Integer.parseInt(thirdByte,2));
+	   
+	 
+
+	   
+		//crude implementation with majority packet hardcoded
+		//incorporate NASdefinitions [specifically single bit flags etc in next step]
+		String Packet = firstbyte +secondbyte +thirdByte + value1;
+		 
+		//System.out.println( firstbyte );
+		// System.out.println(secondbyte );
+		// System.out.println( thirdByte);
+		// System.out.println( value1 );
+		 System.out.println( "Attach Request Packet:" + " " +Packet );
+		}
+
+ public String [] MMEResponseParser(String Response)
+ {
+	 String Responsebytefirst=Response.substring(0,2);
+	 String Responsebytesecond=Response.substring(2,4 );
+	 
+	 String AuthRejPar,AuthFailPar,SecModeRejPar,AuthReqPar,SecModeCmdPar;
+	 AuthRejPar=null; AuthFailPar=null;SecModeRejPar=null;AuthReqPar=null;SecModeCmdPar=null;
+	
+	 if(Responsebytefirst.equals("07")){
+
+	 if (Responsebytesecond.equals("52")){
+			 
+			 AuthReqPar=Responsebytesecond;
+			// Output[0]=AuthReqPar;
+	 } 
+	 if(Responsebytesecond.equals("54") )
+	 {
+		 AuthRejPar=Responsebytesecond;
+		 //Output[1]=AuthRejPar;
+	 }
+	 else if (((Responsebytesecond.equals("5C"))||(Responsebytesecond.equals("5c"))))
+	 {
+		 AuthFailPar=Responsebytesecond;
+		 //Output[2]=AuthFailPar;
+	 }
+	 else if (((Responsebytesecond.equals("5F"))||(Responsebytesecond.equals("5f"))))
+	 {
+		SecModeRejPar= Responsebytesecond;
+		//Output[4]=SecModeRejPar;
+	 }
+	 
+	 }
+	 
+	 else if (Responsebytesecond.equals("37")){
+		 
+		 String ResponsebyteEight=(Response.substring(15,17));
+		 if(ResponsebyteEight.equals("93") )
+		 {
+			 SecModeCmdPar=ResponsebyteEight;
+			 //Output[5]=SecModeCmdPar;
+		 }
+		 
+	 }
+	 String Output [] ={AuthRejPar,AuthFailPar,SecModeRejPar,AuthReqPar,SecModeCmdPar };//parameters to be set
+	 return Output;
+ }
+ 
+ public String NASAuthenticationResponse(String RES,String AuthRespLength)
+ {
+	 
+	 String firstbyte=(NASDefinitions.SecurityHeaderType.PlainNASMessage.getHexCode()
+			 + NASDefinitions.ProtocolDiscriminatorValue.EPSMobilityManagementMessage.getHexCode()); 
+	 
+	 firstbyte='0'+Integer.toHexString(Integer.parseInt(firstbyte,2));
+	 
+	 String secondbyte= NASDefinitions.MobilityManagementMessageType.AuthenticationResponse.getHexCode(); 
+		
+	 secondbyte=Integer.toHexString(Integer.parseInt(secondbyte,2));
+	 
+	 String AuthRespParameter= RES + AuthRespLength;
+	 
+	 
+	 String Packet=firstbyte + secondbyte + AuthRespParameter;
+	 
+	 
+	 //System.out.println( firstbyte );
+	// System.out.println(secondbyte );
+	// System.out.println(  AuthRespParameter );
+	 System.out.println( "Authentication Response Packet:" + " " +Packet );
+	 return Packet;
+	 
+ }
+
+ 
+ public String NASSecurityModeComplete(String packetval)
+ {
+	 String value1=packetval;
+	 
+	 String Packet=(NASDefinitions.SecurityHeaderType.IntegrityProtectedwithnewEPSSecurityContext.getHexCode()
+				+ NASDefinitions.ProtocolDiscriminatorValue.MobilityManagementMessage.getHexCode()
+				+ NASDefinitions.MobilityManagementMessageType.SecurityModeComplete.getHexCode()
+				+value1);
+	
+	 return Packet;
+ }
+ 
+ public String NASESMInformationResponse(String packetval)
+ {
+	 String value1=packetval;
+	 
+	 String Packet=(NASDefinitions.SecurityHeaderType.IntegrityProtectedCiphered.getHexCode()
+				+ NASDefinitions.ProtocolDiscriminatorValue.EPSSessionManagementMessage.getHexCode()
+				+ NASDefinitions.EPSSessionManagementMessageType.ESMInformationResponse.getHexCode()
+				+value1);
+	 
+	 System.out.println("ESM Information Response Packet:" + " " +Packet);
+	 return Packet;
+	 
+
+ }
+
+ public String NASAttachComplete(String packetval)
+	{
+		
+		
+	 //String value1="710BF600F1100001011234567802C0C000050201D031D15200F1100001"; //representing a particular UE
+		
+	    String value1=packetval;
+		//crude implementation with majority packet hardcoded
+		//incorporate NASdefinitions [specifically single bit flags etc in next step]
+		String Packet =  (NASDefinitions.SecurityHeaderType.IntegrityProtectedCiphered.getHexCode()
+		+ NASDefinitions.ProtocolDiscriminatorValue.MobilityManagementMessage.getHexCode()
+		+ NASDefinitions.MobilityManagementMessageType.AttachComplete.getHexCode()
+		+value1);		
+
+		System.out.println(Packet);
+		return Packet;
+	}
+ 
+
+ public void runNASimulation() throws IOException{
+ 
+	 //UE generated parameters 
+     String Filepath="configuration/SuccessfulAttach.txt"	 ;
+	 
+	 String AttachReq=null;
+	 String AuthReq=null;//from MME;
+	 String AuthResp=null;
+	 String AuthRej=null;
+	 String AuthFail=null;
+	 String SecureModeCmd=null;
+	 String SecurityModeRej=null;
+	 String AttachRej=null;
+	 String ESMInfoReq=null;
+	 String ESMInfoResp=null;
+	 String AttachComplete=null;
+	 String RES="634F82417968CA98"; //UE generated Authentication response parameter information element parameters : RES value and length of auth response
+	 String AuthRespLength="8";
+	 
+	NASImplementation NASobject= new NASImplementation();
+	
+	NASobject.parametersetter(Filepath);
+	//NASobject.NASInit(AttachReq, AuthReq, AuthRespLength, AuthRej, AuthFail, SecurityModeRej, SecureModeCmd, AttachRej, ESMInfoReq, ESMInfoResp, AttachComplete);//redundant
+	
+	 AttachReq=NASobject.AttachRequest;//include only the parts not in definitions
+	 AuthReq=NASobject.AuthRequest;//include whole packet, will be EPC generated, only used at UE for checking
+	 AuthResp=NASobject.AuthResponse;//include only parts not in definitons
+	 AuthRej=NASobject.AuthReject;//complete packet,from MME,for checking
+	 AuthFail=NASobject.AuthFailure;//complete packet,from MME,for checking
+	 SecureModeCmd=NASobject.SecureModeCommand;//
+	 SecurityModeRej=NASobject.SecurityModeReject;//complete,for checking
+	 AttachRej=NASobject.AttachReject;
+	 ESMInfoReq=NASobject.ESMInfoRequest;
+	 ESMInfoResp=NASobject.ESMInfoResponse;
+	 AttachComplete=NASobject.AttachComplete;
+	 
+	// System.out.println(AuthReq);
+	  NASobject.NASAttachRequest(AttachReq);  
+	 
+	 /*//wait for MME response i.e.AuthReq message expected
+	   System.out.println(AuthRej);
+	   System.out.println(AuthFail);
+	   System.out.println(AuthReq); *///Debugging Comments 
+	 String [] ParserOutput;
+	
+	 
+	 /*assuming no authentication reject message received.If however
+	 an Authentication Reject message is received, the UE shall abort any EMM signalling procedure 
+	 and enter state EMM-DEREGISTERED.*/
+	 
+	 if((NASobject.AuthReject.equals("null")&& (NASobject.AuthFailure.equals("null"))&& !(AuthReq.equals(null))))
+	 {
+		 
+		 System.out.println("Authentication Request from UE :" + " " +NASobject.ESMInfoRequest);
+		 NASobject.NASAuthenticationResponse(RES, AuthRespLength);
+		 System.out.println("UE Authentication Response Sent to MME.");
+		 
+	 /*(When MME gets the authentication response message the MME compares the received RES value with the XRES (Expected Response) value. 
+	 If RES == XRES, successful authentication*/
+	 	 
+	 }
+	 
+	// when AuthReq =null i.e. Auth Reject or Auth Failure
+ 
+	 else if(!((AuthRej.equals("null"))) )
+	 {
+		 ParserOutput= NASobject.MMEResponseParser(NASobject.AuthReject);
+		 if(ParserOutput[0].equals("54"))
+		 {
+			 //System.out.print(ParserOutput[0]);
+			 System.out.println("Authentication not Completed.Authentication Reject Packet Received!");
+		 }
+		  
+	 }
+	 else if(!(AuthFail.equals("null")))
+	 {
+		 ParserOutput= NASobject.MMEResponseParser(AuthFail);
+		 //System.out.print(ParserOutput[1]);                                  //Debugging Comment
+		 if (((ParserOutput[1].equals("5C"))||(ParserOutput[1].equals("5c"))))
+		 {
+			 System.out.println("Authentication not Completed.Authentication Failure Packet Received!");
+		 }
+	 }
+	 
+	 //wait for security mode command from MME  
+	 
+	 
+	 if (!(NASobject.SecurityModeReject.equals("null")))
+	 {
+		 ParserOutput= NASobject.MMEResponseParser(NASobject.SecurityModeReject);
+		 if(ParserOutput[2].equals("5f")|| ParserOutput[2].equals("5F"))
+		 {
+		 System.out.println("Security Mode not completed.Security Mode Reject Message Received from MME.");
+		 System.out.println("Security Mode Reject Packet:" + " " + NASobject.SecurityModeReject);	 
+	 }
+	 }
+	 else if((NASobject.SecurityModeReject.equals("null"))&& !(NASobject.SecureModeCommand .equals(null)))
+	 {
+		
+		 System.out.println("Secure Mode Command Received from MME:" + " " + NASobject.SecureModeCommand);
+		 System.out.println ("Waiting for ESM information Request from MME...");
+	 }
+	 
+	 
+	 //wait for ESM Information Request from MME
+	 
+	 if (!(NASobject.ESMInfoRequest.equals("null")))
+	 {
+		 System.out.println("ESM Information Request Received from MME:" + "\n" + "ESM Info Request packet:" + " " + NASobject.ESMInfoRequest);
+		 NASobject.NASESMInformationResponse(ESMInfoResp);  //send ESM Information Response
+	 }
+
+	 //Wait for attach Accept/Complete packet from MME
+	 
+	 
+	 if(!(NASobject.AttachReject.equals("null")))
+	 {
+		 System.out.println("Attach Rejected");
+	 }
+	 else if(!(NASobject.AttachComplete.equals("null")) && NASobject.AttachReject.equals("null"))
+	{
+		System.out.println("UE attached succesfully:" ) ;
+		System.out.print("Attach Complete Packet Received from UE:" + " " );
+		NASobject.NASAttachComplete(AttachComplete);
+		
+	}
+	
+	
+	 
+ } 
+} 
+
