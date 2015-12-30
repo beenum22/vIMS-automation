e366037fb65194be686548f3ad9de00a462b975c
Aamir Khan <aamir.fastnu@gmail.com>
2015-12-30 12:40:05 +0500

    Added the IMSIParser utility to the code

--- src/testing/main.java
+++ src/testing/main.java
@@ -2,6 +2,7 @@ package testing;
 
 import java.io.File;
 
+import com.xflowresearch.nfv.testertool.common.IMSIParser;
 import com.xflowresearch.nfv.testertool.simulationcontrol.SimulationControl;
 
 public class main
@@ -12,7 +13,10 @@ public class main
 		/** Delete logs from previous launches **/
 		File index = new File("logs");
 		String [] entries = index.list();
-
+		
+		/*IMSIParser parser = new IMSIParser();
+		parser.createIMSIFile("data.txt", "configuration/IMSIParameters.xml");*/
+		
 		if(entries != null)
 		{
 			for(String s:entries)
