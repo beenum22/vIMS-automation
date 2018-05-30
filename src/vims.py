89c0eabd72c502785dabeb8dbd233c2459a87272
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2018-05-30 05:49:37 +0000

    Reduced flavor resources for development.

--- src/vims.py
+++ src/vims.py
@@ -68,7 +68,7 @@ class vIMS(Stack):
                 'image_name'], url=self.settings.universal['image_url'])
             self.create_keypair(self.settings.universal['keypair_name'])
             self.create_flavor(self.settings.universal[
-                               'flavor_name'], ram=4096, vcpus=2, disk=40)
+                               'flavor_name'], ram=1024, vcpus=1, disk=40)
             self.check_quotas(security_groups=30, floating_ips=50)
             public_net_id = self.get_net_id(
                 self.settings.universal['public_network'])
